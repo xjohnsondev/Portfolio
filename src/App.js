4df4388702e328a1e7951ccfc19b452b20081692
Xavier J <xjohnsondev@macbook-pro.mynetworksettings.com>
2024-04-25 12:38:51 -0700

    starting

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src='../image/world-map.png' alt='test'></img>
+    </div>
+  );
+}
+
+export default App;
